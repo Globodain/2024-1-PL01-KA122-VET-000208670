7bfa739160b82d9d9b64fc8964960d61b756e4d1
JustSG <szymonfujawa.07@gmail.com>
2025-05-12 10:58:18 +0200

    chapters 1 - 8.5

--- app/forms.py
+++ app/forms.py
@@ -35,3 +35,14 @@ class EditProfileForm(FlaskForm):
     username = StringField('Nazwa użytkownika', validators=[DataRequired()])
     about_me = TextAreaField('O mnie', validators=[Length(min=0, max=140)])
     submit = SubmitField('Potwierdź')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = db.session.scalar(sa.select(User).where(
+                User.username == username.data))
+            if user is not None:
+                raise ValidationError('Proszę użyć innej nazwy użytkownika.')
