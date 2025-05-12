69aedcbfceac69363e6e058b51619b9993b4ac6a
KarixD2137 <karol.kopczynski7@gmail.com>
2025-05-12 09:08:29 +0200

    Chapter 4 - finished

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
+from datetime import datetime, timezone
 from typing import Optional
-import sqlalchemy as sa
-import sqlalchemy.orm as so
+import sqlalchemy as sa # elements for editing database
+import sqlalchemy.orm as so # elements for database structure
 from app import db
 
 class User(db.Model):
@@ -8,6 +9,20 @@ class User(db.Model):
   username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique = True)
   email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique = True)
   password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+  posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+  # so.mapped_column() - adds additional properties to columns
+  # Optional[] - allows column to be empty or nullable
 
+  # tells Python how to print object like toString() in Java
   def __repr__(self):
     return '<User {}>'.format(self.username)
+  
+class Post(db.Model):
+  id: so.Mapped[int] = so.mapped_column(primary_key=True)
+  body: so.Mapped[str] = so.mapped_column(sa.String(140))
+  timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
+  user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+  author: so.Mapped[User] = so.relationship(back_populates='posts')
+
+  def __repr__(self):
+    return '<Post {}>'.format(self.body)
