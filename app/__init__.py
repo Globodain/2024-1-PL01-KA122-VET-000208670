0a0b8e05b6d4886f927504cbc52414d5030ce13d
JustSG <szymonfujawa.07@gmail.com>
2025-05-13 19:23:45 +0200

    chapters 1 - 10 and chapter 23

--- app/__init__.py
+++ app/__init__.py
@@ -45,3 +45,5 @@ if not app.debug:
     app.logger.info('Microblog startup')
 
 from app import routes, models, errors
+from app.api import bp as api_bp
+app.register_blueprint(api_bp, url_prefix='/api')
