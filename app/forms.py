4e1277a50d3d0e3e3aac557b83d615efd838b19e
barto3214 <bartek.slusarz@onet.pl>
2025-05-13 11:42:57 +0200

    Chapter 9 ended

--- app/forms.py
+++ app/forms.py
@@ -45,3 +45,8 @@ class EditProfileForm(FlaskForm):
             
 class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
+    
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[
+        DataRequired(), length(min=1, max=140)])
+    submit = SubmitField('Submit')
