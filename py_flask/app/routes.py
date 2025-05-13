e2844033ac4c4a81e58971929b1a6363bc80cb5b
Pawel Wasilewski <wasilewski.pawel2007@gmail.com>
2025-05-13 12:52:21 +0200

    start of part 20

--- py_flask/app/routes.py
+++ py_flask/app/routes.py
@@ -159,4 +159,10 @@ def reset_password_request():
         flash('Check your email for the instructions to reset your password')
         return redirect(url_for('login'))
     return render_template('resetpasswordrequest.html', title='Reset Password', form=form)
+@bp.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    form = EmptyForm()
+    return render_template('userpopup.html', user=user, form=form)
     
