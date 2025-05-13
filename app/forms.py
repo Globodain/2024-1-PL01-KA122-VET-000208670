9000f2be1197edbb2469202070f3c769f084a737
palstr <palstrgames@gmail.com>
2025-05-13 09:51:44 +0200

    CHAPTER IX

--- app/forms.py
+++ app/forms.py
@@ -52,3 +52,8 @@ class EditProfileForm(FlaskForm):
             
 class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
+
+class PostForm(FlaskForm):
+    post = TextAreaField('MÓW.', validators=[
+        DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
